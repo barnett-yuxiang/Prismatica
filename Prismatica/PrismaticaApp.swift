5f74f18d60fe7cf24af79973c827fe8c72a381bd
yuli.kamakura <barnett.yuxiang@gmail.com>
2025-02-13 10:14:50 +0800

    Initial Commit

---
+++ Prismatica/PrismaticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismaticaApp.swift
+//  Prismatica
+//
+//  Created by yuxiang on 2025/2/13.
+//
+
+import SwiftUI
+
+@main
+struct PrismaticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrismaticaTests/PrismaticaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrismaticaTests.swift
+//  PrismaticaTests
+//
+//  Created by yuxiang on 2025/2/13.
+//
+
+import Testing
+@testable import Prismatica
+
+struct PrismaticaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
